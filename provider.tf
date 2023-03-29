f82ff8d22f2cf36025acdd132541c966f586cac8
ArunWadhwagit <74895789+ArunWadhwagit@users.noreply.github.com>
2023-03-29 17:38:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "azurerm" {
   features {}
   subscription_id = "36b20a16-4bd9-4cfb-b3d7-5aef3477cd5f" 
   tenant_id         = "09bed70f-d1c4-4bac-9f1b-c8a9407e0f8e"
-  client_id         = "80fddc77-8e57-43bd-87e8-ff873ad5e096"
-  client_secret     = "9v78Q~8Nvg3y8y1iPnZtRXfadgc~hnu.GbvYNbS5"
+  client_id         = "ab20817f-9f6d-4033-8422-fa77d9de441b"
+  client_secret     = "N2R8Q~pGVPe9WDVdNBQ6T6ZVYdzYoV_qVPRSjc2f"
 }
